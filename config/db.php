62543c175817868ae8dddb43a42dbb843d6c47ac
awwar <saysdragon007@gmail.com>
2020-12-26 16:47:40 +0300

    feature: add db.env to app service

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=db;dbname=' . getenv('myapi'),
+    'dsn' => 'mysql:host=db;dbname=' . getenv('MYSQL_DATABASE'),
     'username' => getenv('MYSQL_USER'),
     'password' => getenv('MYSQL_PASSWORD'),
     'charset' => 'utf8',
